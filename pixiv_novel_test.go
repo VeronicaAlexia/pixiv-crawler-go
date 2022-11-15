7c1982f48559dec67e32b02a027c99738d70f00b
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-15 21:52:58 +0800

    ~

--- pixiv_novel_test.go
+++ pixiv_novel_test.go
@@ -35,10 +35,16 @@ func TestPixivNovel(t *testing.T) {
 	if response, err := app.App.NovelDetail("18729784"); err != nil {
 		t.Error(err)
 	} else {
-		book_info := ""
-		book_info += "title: " + response.Novel.Title + "\n"
-		book_info += "id: " + fmt.Sprint(response.Novel.ID) + "\n"
-		book_info += "uploaddate: " + response.Novel.CreateDate.Format("2006-01-02 15:04:05") + "\n"
+		book_info := "title: " + response.Novel.Title + "\n"
+		book_info += "author: " + response.Novel.User.Name + "\n"
+		book_info += "novel_id: " + fmt.Sprint(response.Novel.ID) + "\n"
+		book_info += "update: " + response.Novel.CreateDate.Format("2006-01-02 15:04:05") + "\n"
+		book_info += "intro: " + response.Novel.Caption + "\n"
+		book_info += "tags: "
+		for _, tag := range response.Novel.Tags {
+			book_info += tag.Name + ", "
+		}
+		book_info += "\n"
 		file.Open("novel/"+response.Novel.Title+".txt", "w", book_info+"\n"+app.App.NovelContent("18729784"))
 
 	}
